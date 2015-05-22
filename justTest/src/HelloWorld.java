320dd8d6bcbd1f8d57f72aee82dfbe4fa6f49b17
Hoàng Hà <hahvie@gmail.com>
2015-05-22 07:42:46 +0700

    put some enters

---
+++ justTest/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	public void say1(){
+		System.out.println("Hello World!");
+	}
+
+	public void sayHi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void say() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+}
---
+++ justTest/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void Main(String[] args){
+		HelloWorld hi = new HelloWorld();
+		hi.say();
+		
+		
+		
+		
+	}
+}
